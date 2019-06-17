3d9821350f0ce15a0e16ef85a60b115fdae8a598
Faiq Allam <faiq.kaboel@gmail.com>
2019-06-18 01:08:03 +0700

    Add Vuex with state of token, user and isLoggedIn

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,35 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+    state: {
+        token: null,
+        user: null,
+        isLoggedIn: false,
+    },
+    getters: {
+        getStatus: state => {
+            return state.isLoggedIn;
+        },
+        getToken: state => {
+            return state.token;
+        },
+        getUser: state => {
+            return state.user;
+        }
+    },
+    mutations: {
+        Login(state, data) {
+            state.token = data.token;
+            state.user = data.user;
+            state.isLoggedIn = state.token !== null
+        }
+    },
+    actions: {
+        Login({commit}, data) {
+            commit('Login', data);
+        }
+    }
 })
