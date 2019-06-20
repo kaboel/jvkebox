56ea11808706d67c31ce4638886790b3964fef99
Faiq Allam <faiq.kaboel@gmail.com>
2019-06-20 07:14:32 +0700

    Add Panel for helper component

---
+++ client/src/components/helpers/Panel.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="songs">
+        <div class="white elevation-2">
+            <v-toolbar flat dense class="deep-purple" dark>
+                <v-toolbar-title>{{ title || "No title defined." }}</v-toolbar-title>
+            </v-toolbar>
+
+            <slot>
+                <div style="text-align: center" class="pt-3 pb-3">No content defined.</div>
+            </slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'panel',
+        props: {
+            title: String
+        }
+    }
+</script>
+
+<style scoped>
+</style>
