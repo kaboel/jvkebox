d6dbc5b00d198c7d2f8d627d7644e0046a1ac675
Faiq Allam <faiq.kaboel@gmail.com>
2019-06-20 07:12:30 +0700

    Add Song model

---
+++ server/src/models/Song.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('Song', {
+        title: DataTypes.STRING,
+        artist: DataTypes.STRING,
+        genre: DataTypes.STRING,
+        album: DataTypes.STRING,
+        albumImageUrl: DataTypes.STRING,
+        youtubeId: DataTypes.STRING,
+        lyrics: DataTypes.TEXT,
+        tab: DataTypes.TEXT,
+    });
+};
