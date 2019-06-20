c4c068cc947d9abc9d93fb624a1f275181fa5553
Faiq Allam <faiq.kaboel@gmail.com>
2019-06-20 10:12:27 +0700

    Add API's for update, delete, and get single song

--- client/src/services/SongsService.js
+++ client/src/services/SongsService.js
@@ -3,5 +3,17 @@ import Api from './Api';
 export default {
     loadSongs() {
         return Api().get('/songs');
+    },
+    addSong(song) {
+        return Api().post('/songs', song);
+    },
+    updateSong(id, song) {
+        return Api().patch('/song/update', id, song);
+    },
+    deleteSong(id) {
+        return Api().delete('/song/delete', id);
+    },
+    showSong(id) {
+          return Api().get('/song', id);
     }
 }
