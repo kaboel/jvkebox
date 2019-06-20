153f94082536dae7710d708b3f367364a0cbcd9c
Faiq Allam <faiq.kaboel@gmail.com>
2019-06-20 07:12:47 +0700

    Add Songs Controller

---
+++ server/src/controllers/SongsController.js
@@ -0,0 +1,27 @@
+const { Song } = require('../models');
+
+module.exports = {
+    async load(req, res) {
+        await Song.findAll({
+            limit: 10
+        }).then(songs => {
+            res.send(songs);
+        }).catch(err => {
+            res.status(500).send({
+                error: `Failed to retrieve songs - ${err.message}.`
+            })
+        })
+    },
+
+    async create(req, res) {
+        await Song.create(req.body)
+            .then(song => {
+                res.send(song.toJSON());
+            })
+            .catch(err => {
+                res.status(500).send({
+                    error: `Failed to add new song entry - ${err.message}.`
+                })
+            })
+    }
+};
