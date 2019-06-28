35d97a021508972d9c7478beabee1176f4c253e9
Faiq <faiq.kaboel@gmail.com>
2019-06-28 11:27:14 +0700

    Add new method to find song by primary key (id)

--- server/src/controllers/SongsController.js
+++ server/src/controllers/SongsController.js
@@ -23,5 +23,20 @@ module.exports = {
                     error: `Failed to add new song entry - ${err.message}.`
                 })
             })
+    },
+
+    async find(req, res) {
+        await Song.findByPk(req.params.id)
+            .then(song => {
+                if(song !== null) {
+                    res.send(song.toJSON());
+                }
+                res.status(400).send({ error: `Song not found.`})
+            })
+            .catch(err => {
+                res.status(500).send({
+                    error: `Failed to add new song entry - ${err.message}.`
+                })
+            })
     }
 };
