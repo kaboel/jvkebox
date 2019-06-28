c56796162bd51f6f587a828d52c053ffe2b1947f
Faiq <faiq.kaboel@gmail.com>
2019-06-28 11:24:55 +0700

    Add API request to find song by id

--- client/src/services/SongsService.js
+++ client/src/services/SongsService.js
@@ -2,18 +2,18 @@ import Api from './Api';
 
 export default {
     loadSongs() {
-        return Api().get('/songs');
+        return Api().get(`/songs`);
     },
     addSong(song) {
-        return Api().post('/songs', song);
-    },
-    updateSong(id, song) {
-        return Api().patch('/song/update', id, song);
-    },
-    deleteSong(id) {
-        return Api().delete('/song/delete', id);
+        return Api().post(`/songs`, song);
     },
     showSong(id) {
-          return Api().get('/song', id);
+        return Api().get(`/songs/${id}`);
+    },
+    updateSong(id, song) {
+        return Api().put(`/songs/${id}`, song);
+    },
+    deleteSong(id) {
+        return Api().delete(`/song/delete`, id);
     }
 }
